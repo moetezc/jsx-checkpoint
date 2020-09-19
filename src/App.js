0eb0a90f4f0b910d5f93cdd0b0ff84bab6f37fba
moetezc <moetezc@gmail.com>
2020-09-19 01:03:25 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import myImage from './imageInSrc.jpg';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+    <h1 className="title red">Moetez Chniti</h1>
+    <img src={myImage} width="300" /> <br></br>
+    <img src="/imageInPublic.jpg" width="300"/>  
     </div>
-  );
+    <video width="320" height="240" controls>
+    <source src="myVideo.mp4" type="video/mp4"/> 
+    </video>
+  </div>
+);
 }
 
 export default App;
